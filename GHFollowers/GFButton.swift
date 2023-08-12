132d3a50a2990ea46d639d94f61a0e9317ac8649
Joel Storr <joel.storr@ewe.net>
2023-08-12 11:24:14 +0200

    Added Custom Button

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,43 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Joel Storr on 12.08.23.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    //Handles Inits via Stroyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(backgroundColor: UIColor, title: String){
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    
+    
+    private func configure(){
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        //When you create you own component, this enables autolayout
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+    
+}
