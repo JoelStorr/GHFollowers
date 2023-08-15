e8385503d87d8d046417946501b9249281b8487a
Joel Storr <joel.storr@ewe.net>
2023-08-15 22:01:05 +0200

    Added Message Label

--- GHFollowers/GFAlertVC.swift
+++ GHFollowers/GFAlertVC.swift
@@ -42,6 +42,8 @@ class GFAlertVC: UIViewController {
         view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
         configureContainerView()
         configureTitleLabel()
+        configureActionButton()
+        configureMessageLabel()
     }
     
     
@@ -82,6 +84,51 @@ class GFAlertVC: UIViewController {
         
         
     }
+    
+    
+    func configureActionButton(){
+        containerView.addSubview(actionButton)
+        
+        actionButton.setTitle(buttonTitle ?? "OK", for: .normal)
+        
+        actionButton.addTarget(self, action: #selector(dismissVC), for: .touchUpInside)
+        
+        
+        NSLayoutConstraint.activate([
+            actionButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+        
+    }
+    
+    
+    func configureMessageLabel(){
+        containerView.addSubview(messageLabel)
+        messageLabel.text = message ?? "Unable to complete request"
+        messageLabel.numberOfLines = 4
+        
+        NSLayoutConstraint.activate([
+            messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            messageLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+            messageLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+            messageLabel.bottomAnchor.constraint(equalTo: actionButton.topAnchor, constant: -12)
+            
+        
+        ])
+        
+        
+    }
+    
+    
+    
+    
+    @objc func dismissVC(){
+        dismiss(animated: true)
+    }
+    
+    
     
 
     
