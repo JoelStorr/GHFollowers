c889e2203c753748226e1dbc0b850cc008a5e98d
Joel Storr <joel.storr@ewe.net>
2023-08-15 22:10:28 +0200

    Added error message popup when no name was given

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -44,7 +44,7 @@ class SearchVC: UIViewController {
     @objc func pushFollowerListVC(){
         
         guard isUserNameEntered else {
-            print("No username")
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Pleas enter a username, we need to know who to look for 😀.", buttonTitle: "Ok")
             return }
         
         let followerListVC = FollowerListVC()
