d1ebf43238b43cadd8f1a118017edbb584d71212
Joel Storr <joel.storr@ewe.net>
2023-08-16 10:03:20 +0200

    Added Models for network call

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Joel Storr on 16.08.23.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
