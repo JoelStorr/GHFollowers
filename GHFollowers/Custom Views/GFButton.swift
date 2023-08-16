f84a3dc3c4cb01f419fd3fc7f6f889b97f622cec
Joel Storr <joel.storr@ewe.net>
2023-08-16 15:49:59 +0200

    Fixed styling bug

--- GHFollowers/Custom Views/GFButton.swift
+++ GHFollowers/Custom Views/GFButton.swift
@@ -32,7 +32,7 @@ class GFButton: UIButton {
     
     private func configure(){
         layer.cornerRadius = 10
-        titleLabel?.textColor = .white
+        setTitleColor(.white, for: .normal)
         titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
         //When you create you own component, this enables autolayout
         translatesAutoresizingMaskIntoConstraints = false
