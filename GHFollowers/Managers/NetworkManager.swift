f95af1a4b2f91826ccf77431102fcde269ffb94e
Joel Storr <joel.storr@ewe.net>
2023-08-16 10:41:00 +0200

    Added the network handler

---
+++ GHFollowers/Managers/NetworkManager.swift
@@ -0,0 +1,44 @@
+//
+//  NetworkManager.swift
+//  GHFollowers
+//
+//  Created by Joel Storr on 16.08.23.
+//
+
+import Foundation
+
+class NetworkManager{
+    
+    let shared = NetworkManager()
+    let baseUrl = "https://api.github.com/users/"
+    
+    private init(){}
+    
+    
+    func getFollowers(for username: String, page: Int, completed: @escaping([Follower]?, String?)->Void){
+        let endpoint = baseUrl + ":\(username)/followers?per_page=100&page=\(page)"
+        
+        
+        guard let url = URL(string: endpoint) else {
+            completed(nil, "This username created an invalid request. Please try again")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url){ data, response, error in
+            
+            if let _ = error{
+                completed(nil, "Unable to complete your request. Please check your Internet connection.")
+            }
+            
+            
+            guard let respnse = response as? HTTPURLResponse, respnse.statusCode == 200 else {
+                
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+}
