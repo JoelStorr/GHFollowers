f2fdba6c2324fa5c640d369b807d7984440b1c22
Joel Storr <joel.storr@ewe.net>
2023-08-17 11:47:20 +0200

    Added scrollable grid layout

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -10,8 +10,18 @@ import UIKit
 class FollowerListVC: UIViewController {
 
     
+    enum Section{
+        case main
+    }
+    
+    
     var username: String!
+    var followers: [Follower] = []
+    
+    
+    
     var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
     
     override func viewDidLoad() {
@@ -19,6 +29,7 @@ class FollowerListVC: UIViewController {
         configureViewController()
         configureCollectionView()
         getFollowers()
+        configureDataSource()
         
     }
     
@@ -38,15 +49,37 @@ class FollowerListVC: UIViewController {
     
     
     func configureCollectionView(){
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
         
-        collectionView.backgroundColor = .systemRed
+        collectionView.backgroundColor = .systemBackground
         
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
         
     }
 
+    
+    func createThreeColumnFlowLayout()-> UICollectionViewFlowLayout{
+        
+        //Calculates the width of a cell
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let avalibleWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = avalibleWidth / 3
+        
+        
+        //Builds the Layout
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        
+        
+        
+        return flowLayout
+    }
+    
     
     
     
@@ -55,8 +88,8 @@ class FollowerListVC: UIViewController {
             
             switch result {
             case .success(let followers):
-                print("Followers.count = \(followers.count)")
-                print(followers)
+                self.followers = followers
+                self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad stuff happend", message: error.rawValue, buttonTitle: "Ok")
             }
@@ -74,6 +107,27 @@ class FollowerListVC: UIViewController {
             print("Followers.count = \(followers.count)")
             print(followers)
             */
+    }
+    
+    
+    func configureDataSource(){
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            
+            return cell
+        })
+    }
+    
+    //Creates a new Snapshot of Data to compare against
+    func updateData(){
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
     }
     
 
