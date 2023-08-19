53b87341b8adc21f42dfb52ed33033965df1882b
Joel Storr <joel.storr@ewe.net>
2023-08-19 11:10:24 +0200

    Added empty state view

---
+++ GHFollowers/Custom Views/Views/GFEmptyStateView.swift
@@ -0,0 +1,66 @@
+//
+//  GFEmptyStateView.swift
+//  GHFollowers
+//
+//  Created by Joel Storr on 19.08.23.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+
+    
+    let messageLabel = GFTitleLabel(textAlignment: .center, fontSize: 28)
+    let logoImageView = UIImageView()
+    
+    
+    
+   override init(frame: CGRect){
+        super.init(frame: frame)
+       configure()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    init(message: String){
+        super.init(frame: .zero)
+        messageLabel.text = message
+        configure()
+    }
+    
+    
+    private func configure(){
+        addSubview(messageLabel)
+        addSubview(logoImageView)
+        
+        messageLabel.numberOfLines = 3
+        messageLabel.textColor = .secondaryLabel
+        
+        logoImageView.image = UIImage(named: "empty-state-logo")
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+        
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
+            messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            messageLabel.heightAnchor.constraint(equalToConstant: 200),
+            
+            //Pushes the Logo to bottom right with only half in frame
+            logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 200),
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 140)
+        
+        ])
+        
+    }
+   
+
+}
