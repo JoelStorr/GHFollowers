230c7ff0d7b66bcf5ec843636fbcfa769d552f09
Joel Storr <joel.storr@ewe.net>
2023-08-21 10:33:29 +0200

    Added the UserInfo Header

--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -31,7 +31,11 @@ class UserInfoVC: UIViewController {
             
             switch result{
             case .success(let user):
-                print(user)
+                
+                DispatchQueue.main.async {
+                    self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+                }
+                
                 
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something went wrong.", message: error.rawValue, buttonTitle: "OK")
@@ -45,19 +49,26 @@ class UserInfoVC: UIViewController {
     
     func layoutUI(){
         view.addSubview(headerView)
-        headerView.backgroundColor = .systemPink
+      
         headerView.translatesAutoresizingMaskIntoConstraints = false
         
         
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             headerView.heightAnchor.constraint(equalToConstant: 180)
         
         ])
     }
     
+    
+    func add(childVC: UIViewController, to containerView: UIView){
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
+    }
     
     
     
