a5a16266f271c800fb58d2974b14bca478e4fd9f
Joel Storr <joel.storr@ewe.net>
2023-09-01 10:53:23 +0200

    Refactored styling for the alert container view

--- GHFollowers/Custom Views/ViewControllers/GFAlertVC.swift
+++ GHFollowers/Custom Views/ViewControllers/GFAlertVC.swift
@@ -9,17 +9,14 @@ import UIKit
 
 class GFAlertVC: UIViewController {
 
-    
-    let containerView = UIView()
+    let containerView = GFAlertContainerView()
     let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
     let messageLabel = GFBodyLabel(textAlignment: .center)
     let actionButton = GFButton(backgroundColor: .systemPink, title: "OK")
     
-    
     var alertTitle: String?
     var message: String?
     var buttonTitle: String?
-    
     let padding: CGFloat = 20
     
     
@@ -30,16 +27,16 @@ class GFAlertVC: UIViewController {
         self.buttonTitle = buttonTitle
     }
     
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
         configureContainerView()
         configureTitleLabel()
         configureActionButton()
@@ -50,49 +47,33 @@ class GFAlertVC: UIViewController {
     func configureContainerView(){
         view.addSubview(containerView)
         
-        containerView.backgroundColor = .systemBackground
-        containerView.layer.cornerRadius = 16
-        containerView.layer.borderWidth = 2
-        containerView.layer.borderColor = UIColor.white.cgColor
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        
-        
         NSLayoutConstraint.activate([
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             containerView.widthAnchor.constraint(equalToConstant: 280),
             containerView.heightAnchor.constraint(equalToConstant: 220)
         ])
-        
-        
     }
+    
     
     func configureTitleLabel(){
         containerView.addSubview(titleLabel)
         
         titleLabel.text = alertTitle ?? "Something went wrong"
         
-        
         NSLayoutConstraint.activate([
-            
             titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: padding),
             titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
             titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
             titleLabel.heightAnchor.constraint(equalToConstant: 28)
-        
         ])
-        
-        
     }
     
     
     func configureActionButton(){
         containerView.addSubview(actionButton)
-        
         actionButton.setTitle(buttonTitle ?? "OK", for: .normal)
-        
         actionButton.addTarget(self, action: #selector(dismissVC), for: .touchUpInside)
-        
         
         NSLayoutConstraint.activate([
             actionButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -padding),
@@ -100,7 +81,6 @@ class GFAlertVC: UIViewController {
             actionButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
             actionButton.heightAnchor.constraint(equalToConstant: 44)
         ])
-        
     }
     
     
@@ -114,23 +94,11 @@ class GFAlertVC: UIViewController {
             messageLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
             messageLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
             messageLabel.bottomAnchor.constraint(equalTo: actionButton.topAnchor, constant: -12)
-            
-        
         ])
-        
-        
     }
     
-    
-    
-    
+
     @objc func dismissVC(){
         dismiss(animated: true)
     }
-    
-    
-    
-
-    
-
 }
